bb15a2d62578906abd5e75ecf161302929c5f41e
Lilith Roth <lilith@roth.systems>
2025-06-03 15:38:44 +0200

    wip

--- src/upload/sftp.rs
+++ src/upload/sftp.rs
@@ -1,5 +1,6 @@
-use std::path::Path;
+use std::{net::TcpStream, path::Path};
 
+use ssh2::Session;
 use url::Url;
 
 
@@ -8,6 +9,7 @@ pub(crate) fn upload_sftp(
     file_path: Box<Path>,
     remote_str: String
 ) {
+    // Parsing remote information from provided remote_str
     let remote_url = Url::parse(&remote_str).expect("Could not parse remote URL!");
     let host = remote_url.host_str().expect("Could not retrieve remote host from URL!");
     let port_raw = remote_url.port();
@@ -15,5 +17,10 @@ pub(crate) fn upload_sftp(
         Some(port) => port,
         None => 21,
     };
+
+
+    // Connect to SSH
+    let tcp = TcpStream::connect(format!("{host}:{port}"));
+    let mut session = Session::new().expect("Could not create SSH session!");
 }
 
