ae25ccde60d5cca5bb04b7978c6d8c4a324d70a7
Lilith Roth <lilith@roth.systems>
2026-05-31 15:29:54 +0200

    refactor: SFTP module

--- src/upload/protocols/sftp.rs
+++ src/upload/protocols/sftp.rs
@@ -1,8 +1,8 @@
 use anyhow::{Context, anyhow};
-use ssh2::{Channel, Error, Session, Sftp};
+use ssh2::{Error, Session, Sftp};
 use std::{
-    fs::{self, File},
-    io::{BufReader, Read},
+    fs::File,
+    io::BufReader,
     net::TcpStream,
     path::{Path, PathBuf},
 };
@@ -11,6 +11,11 @@ use url::Url;
 use crate::Config;
 use crate::upload::utils::file_name::{generate_backup_name, get_backup_name_stem};
 
+/// Establishes connection with the remote SFTP server, uploads backup, and removes old backups.
+///
+/// # Arguments
+/// * `file_path`   - Path to the file to upload
+/// * `config`      - Parsed configuration file
 pub(crate) fn upload_sftp(file_path: &Path, config: &Config) -> anyhow::Result<()> {
     // Parsing remote information from provided remote_str
     let remote_url: Url = Url::parse(&config.remote).context("Could not parse remote URL!")?;
@@ -44,36 +49,40 @@ pub(crate) fn upload_sftp(file_path: &Path, config: &Config) -> anyhow::Result<(
 
     authenticate_ssh(&username, &session, config)?;
 
-    create_remote_directory(remote_path, &session)?;
+    sftp_session.mkdir(&Path::new(remote_path), 0600).context("Could not create remote directory for backups!")?;
     upload_backup(
         remote_path,
         &generate_backup_name(file_path)?,
-        upload_buffer_size,
         file_path,
-        &session,
+        upload_buffer_size,
+        &sftp_session,
     )?;
     delete_old_backups(
-        remote_path,
+        config.amount_of_backups_to_keep,
         &get_backup_name_stem(file_path)?,
+        remote_path,
         &sftp_session,
-        config,
     )?;
     Ok(())
 }
 
+/// Deletes oldest backups on SFTP remote, and keeps N newest
+///
+/// # Arguments
+/// * `n`                   - Amount of backups to keep
+/// * `backup_name_stem`    - Stem of the backup name, used to identify the backups on remote
+/// * `remote_path`         - Path on remote to the directory containing backups
+/// * `sftp_session`        - Established SFTP session
 fn delete_old_backups(
-    remote_path: &str,
+    n: u16,
     backup_name_stem: &str,
+    remote_path: &str,
     sftp_session: &Sftp,
-    config: &Config,
 ) -> anyhow::Result<()> {
-    log::info!("DEL!");
-    log::info!("path: {remote_path:?}");
-    let mut dir_content = sftp_session.readdir(remote_path).unwrap();
-    log::info!("DC: {dir_content:?}");
+    let mut dir_content = sftp_session.readdir(remote_path).context("Could not read remote backup directory content!")?;
     dir_content.sort_by_key(|x| x.0.file_name().unwrap_or_default().to_os_string());
 
-    let mut skip_counter = config.amount_of_backups_to_keep;
+    let mut skip_counter = n;
     for x in &dir_content {
         let file_name =
             x.0.file_name()
@@ -88,72 +97,49 @@ fn delete_old_backups(
             continue;
         }
 
-        log::debug!("DELETE: {x:?}");
+        log::debug!("Deleting old backup: {x:?}");
+        sftp_session.unlink(&*x.0).context("Could not delete old backup: {x:?}")?;
     }
 
     Ok(())
 }
 
-/// Uploads file via SSH
+/// Uploads the backup, to be called by `upload_sftp` above
+///
+/// # Arguments
+/// * `remote_path`         - Path to the backup directory on remote server
+/// * `backup_name`         - File name of the backup to upload
+/// * `file_path`           - Path to the file to upload on local system
+/// * `buffer_size_bytes`   - Size in bytes for the file read buffer
+/// * `sftp_session`        - Established SFTP session
 fn upload_backup(
     remote_path: &str,
     backup_name: &str,
-    buffer_size_bytes: usize,
     file_path: &Path,
-    session: &Session,
+    buffer_size_bytes: usize,
+    sftp_session: &Sftp,
 ) -> anyhow::Result<()> {
     // read file
-    let file_size: u64 = fs::metadata(file_path)
-        .context("Could not get temp file metadata!")?
-        .len();
     let file: File = File::open(file_path).context("Failed to open file to upload!")?;
     let mut buf_reader: BufReader<File> = BufReader::with_capacity(buffer_size_bytes, file);
 
     // Write file to remote
     let remote_file_path: PathBuf = Path::join(Path::new(remote_path), backup_name);
     log::debug!("Uploading to {}", remote_file_path.display());
-    let mut remote_file: Channel = session
-        .scp_send(&remote_file_path, 0o644, file_size, None)
-        .context("Could not start upload!")?;
-    std::io::copy(&mut buf_reader, &mut remote_file)
-        .context("Could not write file to remote host!")?;
-
+    let mut remote_file = sftp_session.create(&remote_file_path).context("Could not create file on remote!")?;
+    std::io::copy(&mut buf_reader, &mut remote_file).context("Could not write file to remote!")?;
+    
     // Closing channel
-    remote_file
-        .send_eof()
-        .context("Error sending EOF to SSH server!")?;
-    remote_file
-        .wait_eof()
-        .context("Error waiting for EOF to SSH server!")?;
-    remote_file.close().context("Error closing SSH channel!")?;
-    remote_file
-        .wait_close()
-        .context("Error waiting for SSH channel closing!")?;
+    remote_file.close().context("Error closing remote file handle!")?;
     Ok(())
 }
 
-fn create_remote_directory(remote_path: &str, session: &Session) -> anyhow::Result<()> {
-    // Create remote path if it does not exist
-    let mut mkdir_cmd_channel: Channel = session
-        .channel_session()
-        .context("Could not open SSH command channel to create directory on remote host!")?;
-    let create_dir_cmd: String = format!("mkdir -p {remote_path}");
-    match mkdir_cmd_channel.exec(&create_dir_cmd) {
-        Ok(_remote_path_creation_result) => {
-            log::debug!("Remote path created successfully!\nCommand: {create_dir_cmd:?}");
-        }
-        Err(err) => {
-            log::debug!("Error: {err:?}");
-            return Err(anyhow!("Could not create remote path!"));
-        }
-    }
-    let mut s: String = String::new();
-    mkdir_cmd_channel
-        .read_to_string(&mut s)
-        .context("Could not read remote backup directory creation response!")?;
-    Ok(())
-}
-
+/// Authenticates the SSH session
+///
+/// # Arguments
+/// * `username`    - Username to log in with
+/// * `session`     - SSH session
+/// * `config`      - Parsed configuration file
 fn authenticate_ssh(username: &str, session: &Session, config: &Config) -> anyhow::Result<()> {
     let settings_config: Config = config.clone();
     let ssh_config_accepted: bool = match settings_config.sftp_public_key_path {
@@ -221,6 +207,12 @@ fn authenticate_ssh(username: &str, session: &Session, config: &Config) -> anyho
     Ok(())
 }
 
+/// Creates SSH session
+///
+/// # Arguments
+/// * `host`                    - Remote host IP/domain
+/// * `port`                    - Port to connect to
+/// * `compression_enabled`     - If connection compression should be enabled
 fn setup_ssh_session(host: &str, port: u16, compression_enabled: bool) -> anyhow::Result<Session> {
     // Connect to SSH
     let tcp: TcpStream =
